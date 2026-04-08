c3032449cebf7ff2b44c4c645908a5b5ba0e84d9
saveliy <savam@MacBook-Pro-saveliy.local>
2026-04-08 15:24:49 +0300

    Сделанная работа

--- trees/include/Treap.hpp
+++ trees/include/Treap.hpp
@@ -1,5 +1,6 @@
-#pragma "once"
+#pragma once
 
+#include <cstddef>
 #include <vector>
 
 class Treap
@@ -15,4 +16,27 @@ public:
     std::vector<int> values() const;
 
     ~Treap();
+
+private:
+    struct Node
+    {
+        int value;
+        int priority;
+        Node* left;
+        Node* right;
+
+        Node(int value, int priority);
+    };
+
+    Node* root_ = nullptr;
+    std::size_t size_ = 0;
+
+    bool contains(Node* node, int value) const;
+    void collect_values(Node* node, std::vector<int>& result) const;
+    void destroy(Node* node);
+
+    void split(Node* node, int value, Node*& left, Node*& right);
+    Node* merge(Node* left, Node* right);
+    Node* insert(Node* node, Node* new_node);
+    Node* remove(Node* node, int value, bool& removed);
 };
