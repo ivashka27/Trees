0682b5a087d6cb7bc263c74b71644bfe54ad988d
atapina-p <st139859@spbu.ru>
2026-04-09 16:53:58 +0300

    created avl tree

--- trees/include/AVLTree.hpp
+++ trees/include/AVLTree.hpp
@@ -15,4 +15,37 @@ public:
     std::vector<int> values() const;
 
     ~AVLTree();
+private:
+    std::size_t tree_size = 0;
+    
+    struct node
+    {
+        int key;
+        unsigned int height;
+        node* left;
+        node* right;
+        node(int k) {key = k; left=right=0; height = 1;}
+    };
+    node* root = nullptr;
+    unsigned int get_h(node* p) const; //высота поддерева для узла p
+    int bfactor(node* p) const; //баланс фактор узла p
+
+    void fixheight(node* p); //восстановление корректного значения поля height
+
+    //повороты
+    node* rotate_r(node* p);
+    node* rotate_l(node* q);
+
+    //балансировка
+    node* balance(node* p);
+
+    node* insert(node* p, int k); //вставка ключа k в дерево с корнем p
+
+    node* findmin(node* p); //поиск узла с минимальным ключом из дерева p
+    node* removemin(node* p); //удаление узла с мин ключом из p
+
+    node* remove(node* p, int k); //удаление ключа k из дерева p
+    void clear(node* p); //удаление всего дерева
+    
+    void in_order(node* p, std::vector<int>& values) const;
 };
