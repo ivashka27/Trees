f2b889476d7a7b47890ed96a85b18ed702e2dfd0
arslan05t <arslantolegenov08@gmail.com>
2026-04-11 02:44:57 +0300

    Implemented SplayTree, added CMake config

---
+++ trees/build/CMakeFiles/splay_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/splay_test.dir/link.d"
+  "CMakeFiles/splay_test.dir/main.cpp.o"
+  "CMakeFiles/splay_test.dir/main.cpp.o.d"
+  "splay_test"
+  "splay_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/splay_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
