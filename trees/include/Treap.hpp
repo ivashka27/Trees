3f6edf9e72627f2b0e77f8a81698a0f11d7bbcaf
barbituzz <tverdohlebovdmitrij2@gmail.com>
2026-04-12 18:17:17 +0300

    Treap.hpp

--- trees/include/Treap.hpp
+++ trees/include/Treap.hpp
@@ -1,18 +1,39 @@
 #pragma "once"
 
+#pragma once
 #include <vector>
+#include <cstddef>
 
 class Treap
 {
 public:
+    Treap();
+    ~Treap();
+
     bool contains(int value) const;
     bool insert(int value);
     bool remove(int value);
 
-    std::size_t size() const;
-    bool empty() const;
-
     std::vector<int> values() const;
 
     ~Treap();
+};
+private:
+    struct Node
+    {
+        int key;
+        int priority;
+        Node *left;
+        Node *right;
+        Node(int k);
+    };
+
+    Node *root;
+    std::size_t count;
+
+    // Внутренние функции
+    static Node *merge(Node *L, Node *R);
+    static void split(Node *t, int key, Node *&L, Node *&R);
+    void clear(Node *t);
+    void inorder(Node *t, std::vector<int> &v) const;
 };
